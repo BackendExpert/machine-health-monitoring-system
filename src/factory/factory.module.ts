9b1c48dfd0f51a950fd4872e7d96e6eb927a762a
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2026-05-16 20:30:04 +0530

    UPdate

---
+++ src/factory/factory.module.ts
@@ -0,0 +1,32 @@
+import { Module } from "@nestjs/common";
+import { MongooseModule } from "@nestjs/mongoose";
+import { AuditLog, AuditLogSchema } from "src/auditlogs/schema/auditlog.schema";
+import { AuthModule } from "src/auth/auth.module";
+import { RoleModule } from "src/role/role.module";
+import { User, UserSchema } from "src/user/schema/user.schema";
+import { Factory, FactorySchema } from "./schema/factory.schema";
+import { Role, RoleSchema } from "src/role/schema/role.schema";
+import { ProductionLine, ProductionLineSchema } from "./schema/productionline .schema";
+import { FactoryController } from "./factory.controller";
+import { FactoryService } from "./factory.service";
+
+@Module({
+    imports: [
+        AuthModule,
+        RoleModule,
+        MongooseModule.forFeature([
+            { name: User.name, schema: UserSchema }, 
+            { name: Role.name, schema: RoleSchema },
+            { name: AuditLog.name, schema: AuditLogSchema },
+            { name: Factory.name, schema: FactorySchema },
+            { name: ProductionLine.name, schema: ProductionLineSchema }            
+        ])
+    ],
+    controllers: [FactoryController],
+    providers: [
+        FactoryService
+    ],
+    exports: [FactoryService]
+})
+
+export class FactoryModule { }
